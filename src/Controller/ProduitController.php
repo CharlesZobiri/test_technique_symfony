ca74211e11c5a9f7225c0744178897746156cc9b
Charleszbr <zobiricharles@gmail.com>
2024-12-04 11:24:49 +0100

    feat : Ajout de la fonctionnalité update d'un produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,7 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Produit;
 use App\Repository\CategorieRepository;
-use App\Repository\ProduitRepository; 
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpFoundation\JsonResponse; 
@@ -56,6 +57,52 @@ class ProduitController extends AbstractController
             'message' => 'Produit créé avec succès',
             'produit' => $produit->toArray()
         ], JsonResponse::HTTP_CREATED);
+    }
+
+
+    #[Route('/api/produit/update/{id}', name: 'upadta_produit', methods: ['PUT'])]
+    public function updateProduit(int $id, Request $request, ProduitRepository $produitRepository, CategorieRepository $categorieRepository, EntityManagerInterface $entityManager): JsonResponse 
+    {
+        $produit = $produitRepository->find($id);
+        if (!$produit) {
+            return $this->json(['error' => 'Produit non trouvé'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
+        $data = json_decode($request->getContent(), true);
+        if (!$data) {
+            return $this->json(['error' => 'Données invalides'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        if (isset($data['nom'])) {
+            $produit->setNom($data['nom']);
+        }
+
+        if (isset($data['description'])) {
+            $produit->setDescription($data['description']);
+        }
+
+        if (isset($data['prix'])) {
+            $produit->setPrix((float)$data['prix']);
+        }
+
+        if (isset($data['categorie'])) {
+            // Si une catégorie est précisée alors on recherche le nom dans la BDD
+            $categorie = $categorieRepository->findOneBy(['nom' => $data['categorie']]);
+
+            if (!$categorie) {
+                return $this->json(['error' => 'Catégorie non trouvée'], JsonResponse::HTTP_BAD_REQUEST);
+            }
+
+            $produit->setCategorie($categorie);
+        }
+
+        $entityManager->flush();
+
+        return $this->json([
+            'message' => 'Produit modifié avec succès',
+            'produit' => $produit->toArray()
+        ], JsonResponse::HTTP_OK);
+
     }
 
 }
